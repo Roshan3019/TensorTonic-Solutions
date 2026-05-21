223dd37cd8b55f51eb3a4078fa5fb28c3429dd72
Roshan Patel <70010123+Roshan3019@users.noreply.github.com>
2026-05-21 11:17:26 +0530

    Add solution for transformers-embedding

---
+++ transformer/transformers-embedding/transformers-embedding.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import math
+def create_embedding_layer(vocab_size: int, d_model: int) -> nn.Embedding:
+    """
+    Create an embedding layer.
+    """
+    embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim = d_model)
+    # Your code here
+    return embedding
+
+def embed_tokens(embedding: nn.Embedding, tokens: torch.Tensor, d_model: int) -> torch.Tensor:
+    """
+    Convert token indices to scaled embeddings.
+    """
+    emb = embedding(tokens)
+    scaled_emb = emb * math.sqrt(d_model)
+    return scaled_emb
